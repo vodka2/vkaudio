3c1c28928e1211ad8d965e64d18dc83b777061a3
vodka2 <vodka2@users.noreply.github.com>
2017-10-13 14:37:20 +0700

    Add new site decryption code

--- site_decr_code.js
+++ site_decr_code.js
@@ -1,50 +1,74 @@
-function e(t) {
-    if (~t.indexOf("audio_api_unavailable")) {
-        var i = t.split("?extra=")[1].split("#")
-          , e = o(i[1]);
-        if (i = o(i[0]),
-        !e || !i)
+function i() {
+    return window.wbopen && ~(window.open + "").indexOf("wbopen")
+}
+function o(t) {
+    if (!i() && ~t.indexOf("audio_api_unavailable")) {
+        var e = t.split("?extra=")[1].split("#")
+          , o = "" === e[1] ? "" : a(e[1]);
+        if (e = a(e[0]),
+        "string" != typeof o || !e)
             return t;
-        e = e.split(String.fromCharCode(9));
-        for (var a, r, l = e.length; l--; ) {
-            if (r = e[l].split(String.fromCharCode(11)),
-            a = r.splice(0, 1, i)[0],
-            !s[a])
+        o = o ? o.split(String.fromCharCode(9)) : [];
+        for (var s, r, n = o.length; n--; ) {
+            if (r = o[n].split(String.fromCharCode(11)),
+            s = r.splice(0, 1, e)[0],
+            !l[s])
                 return t;
-            i = s[a].apply(null, r)
+            e = l[s].apply(null, r)
         }
-        if (i && "http" === i.substr(0, 4))
-            return i
+        if (e && "http" === e.substr(0, 4))
+            return e
     }
     return t
 }
-function o(t) {
+function a(t) {
     if (!t || t.length % 4 == 1)
         return !1;
-    for (var i, e, o = 0, s = 0, r = ""; e = t.charAt(s++); )
-        e = a.indexOf(e),
-        ~e && (i = o % 4 ? 64 * i + e : e,
-        o++ % 4) && (r += String.fromCharCode(255 & i >> (-2 * o & 6)));
-    return r
+    for (var e, i, o = 0, a = 0, s = ""; i = t.charAt(a++); )
+        i = r.indexOf(i),
+        ~i && (e = o % 4 ? 64 * e + i : i,
+        o++ % 4) && (s += String.fromCharCode(255 & e >> (-2 * o & 6)));
+    return s
 }
-var a = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMN0PQRSTUVWXYZO123456789+/="
-  , s = {
+function s(t, e) {
+    var i = t.length
+      , o = [];
+    if (i) {
+        var a = i;
+        for (e = Math.abs(e); a--; )
+            o[a] = (e += e * (a + i) / e) % i | 0
+    }
+    return o
+}
+var r = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMN0PQRSTUVWXYZO123456789+/="
+  , l = {
     v: function(t) {
         return t.split("").reverse().join("")
     },
-    r: function(t, i) {
+    r: function(t, e) {
         t = t.split("");
-        for (var e, o = a + a, s = t.length; s--; )
-            e = o.indexOf(t[s]),
-            ~e && (t[s] = o.substr(e - i, 1));
+        for (var i, o = r + r, a = t.length; a--; )
+            i = o.indexOf(t[a]),
+            ~i && (t[a] = o.substr(i - e, 1));
         return t.join("")
     },
-    x: function(t, i) {
-        var e = [];
-        return i = i.charCodeAt(0),
-        t.split("").forEach(function(o) {
-            e.push(String.fromCharCode(o.charCodeAt(0) ^ i))
+    s: function(t, e) {
+        var i = t.length;
+        if (i) {
+            var o = s(t, e)
+              , a = 0;
+            for (t = t.split(""); ++a < i; )
+                t[a] = t.splice(o[i - 1 - a], 1, t[a])[0];
+            t = t.join("")
+        }
+        return t
+    },
+    x: function(t, e) {
+        var i = [];
+        return e = e.charCodeAt(0),
+        each(t.split(""), function(t, o) {
+            i.push(String.fromCharCode(o.charCodeAt(0) ^ e))
         }),
-        e.join("")
+        i.join("")
     }
 }
